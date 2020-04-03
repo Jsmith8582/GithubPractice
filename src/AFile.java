3ce2cd4e1f8fce5eeac852646acdb42dce6d666e
Josh Smith <Jsmith8582@gmail.com>
2020-04-03 12:47:47 -0600

    Testing it out

--- src/AFile.java
+++ src/AFile.java
@@ -3,7 +3,11 @@ public class AFile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println();
+		System.out.println("Does it still push out?");
+		
+		
+		
+		
 	}
 
 }
