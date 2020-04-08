4c5d1a5b7c4f1c5974ccf112633b520cba1afc19
Josh Smith <Jsmith8582@gmail.com>
2020-04-08 09:28:20 -0600

    Counting practice

--- src/AFile.java
+++ src/AFile.java
@@ -5,6 +5,9 @@ public class AFile {
 		// TODO Auto-generated method stub
 		System.out.println("Does it still push out?");
 		
+		for (int z = 0; z <= 200; z += 15) {
+			System.out.println(z);
+		}
 		
 		
 		
